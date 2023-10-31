338be38a2da3e6d4241875c497c36201f53836c4
Christian <31255729+dabbelju007@users.noreply.github.com>
2023-10-31 10:58:44 +0100

    Update check_ovpn.sh
    Überprüfung der Anzahl von Interfaces eingebaut. Wenn tun* > 1 reboot

--- check_ovpn.sh
+++ check_ovpn.sh
@@ -2,13 +2,22 @@
 
 target=10.8.0.1
 count=$( ping -w 5 -c 1 $target | grep ttl=* | wc -l )
+countif=$( ifconfig | grep -c "^tun" )
+
+echo "Anzahl Interfaces"
+echo $countif
 
 if [ $count -eq 0 ]
 then
-        echo "Restarting VPN"
-        /etc/init.d/openvpn stop
-        /data/openvpn/start.sh
-
+        if [ $countif > 1 ]
+        then
+                echo "Reboot tut gut"
+				sudo reboot
+        else
+                echo "Restarting VPN"
+                /etc/init.d/openvpn stop
+                /data/openvpn/start.sh
+        fi
 else
         echo "VPN is Running"
 fi
